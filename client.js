12722557e262431eaaeebea59631283563beaf54
Sahaj Amatya <sahajamatya1@gmail.com>
2020-12-27 03:32:45 -0600

    changed to custom image

--- client.js
+++ client.js
@@ -1,7 +1,9 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import {ReactInstance} from 'react-360-web';
+import { ReactInstance } from 'react-360-web';
+import WebVRPolyfill from 'webvr-polyfill';
+const polyfill = new WebVRPolyfill();
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -17,7 +19,7 @@ function init(bundle, parent, options = {}) {
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  r360.compositor.setBackground(r360.getAssetURL('photo.jpg'));
 }
 
-window.React360 = {init};
+window.React360 = { init };
