4a74a9e02659e0855c8f8ee6398cd705b8cf7031
Sahaj Amatya <sahajamatya1@gmail.com>
2020-12-27 23:39:49 -0600

    VrButton and State Change

--- index.js
+++ index.js
@@ -1,18 +1,65 @@
 import React from 'react';
 import {
+  asset,
   AppRegistry,
+  Environment,
   StyleSheet,
   Text,
   View,
+  VrButton
 } from 'react-360';
+import house from './data/houseData';
 
 export default class HelloWorldVR extends React.Component {
+  state = {
+    room: house.House.roomName,
+    info: house.House.info,
+    adjacentRooms: house.House.adjacentRooms
+  }
+
+  clickHandler(roomSelection) {
+    this.setState({
+      room: house[`${roomSelection}`].roomName,
+      info: house[`${roomSelection}`].info,
+      adjacentRooms: house[`${roomSelection}`].adjacentRooms
+    });
+    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`));
+  }
+
+  createRoomButtons(adjacentRooms) {
+    let rooms = adjacentRooms;
+    let buttons = [];
+
+    rooms.map(room => (
+      buttons.push(
+        <VrButton key={`${room}` + "-button"} onClick={() => this.clickHandler(room)}>
+          <Text style={{ backgroundColor: 'green' }}>{room}</Text>
+        </VrButton>
+      )
+    ))
+    return buttons;
+  }
+
   render() {
     return (
       <View style={styles.panel}>
         <View style={styles.greetingBox}>
-          <Text style={styles.greeting}>
-            Welcome to React 360
+          <Text>
+            Room Selection
+          </Text>
+          <Text>
+            {this.state.room}
+          </Text>
+          {this.createRoomButtons(this.state.adjacentRooms)}
+        </View>
+
+
+        <View style={styles.greetingBox}>
+          <Text>
+            Room Info
+          </Text>
+          <Text>
+            {this.state.info}
           </Text>
         </View>
       </View>
@@ -26,17 +73,14 @@ const styles = StyleSheet.create({
     width: 1000,
     height: 600,
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row'
   },
   greetingBox: {
     padding: 20,
     backgroundColor: '#000000',
     borderColor: '#639dda',
     borderWidth: 2,
-  },
-  greeting: {
-    fontSize: 30,
   },
 });
 
