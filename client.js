06ca855669ffe2884f830b0cf7b54609a9324b40
Sahaj Amatya <sahajamatya1@gmail.com>
2020-12-28 00:05:08 -0600

    surface panels

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // This file contains the boilerplate to execute your React app.
 // If you want to modify your application's content, start in "index.js"
 
-import { ReactInstance } from 'react-360-web';
+import { ReactInstance, Surface } from 'react-360-web';
 import WebVRPolyfill from 'webvr-polyfill';
 const polyfill = new WebVRPolyfill();
 
@@ -11,6 +11,22 @@ function init(bundle, parent, options = {}) {
     fullScreen: true,
     ...options,
   });
+
+  const buttonsPanel = new Surface(400, 550, Surface.SurfaceShape.Flat);
+  buttonsPanel.setAngle(-0.6, 0.1);
+
+  const infoPanel = new Surface(400, 550, Surface.SurfaceShape.Flat);
+  infoPanel.setAngle(0.6, 0);
+
+  r360.renderToSurface(
+    r360.createRoot('Buttons', {}),
+    buttonsPanel
+  );
+
+  r360.renderToSurface(
+    r360.createRoot('InfoPanel', {}),
+    infoPanel
+  );
 
   // Render your app content to the default cylinder surface
   r360.renderToSurface(
--- index.js
+++ index.js
@@ -10,7 +10,64 @@ import {
 } from 'react-360';
 import house from './data/houseData';
 
-export default class HelloWorldVR extends React.Component {
+export class Buttons extends React.Component {
+  state = {
+    room: house.House.roomName,
+    info: house.House.info,
+    adjacentRooms: house.House.adjacentRooms
+  }
+
+  clickHandler(roomSelection) {
+    this.setState({
+      room: house[`${roomSelection}`].roomName,
+      info: house[`${roomSelection}`].info,
+      adjacentRooms: house[`${roomSelection}`].adjacentRooms
+    });
+    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`));
+  }
+
+  createRoomButtons(adjacentRooms) {
+    let rooms = adjacentRooms;
+    let buttons = [];
+
+    rooms.map(room => (
+      buttons.push(
+        <VrButton key={`${room}` + "-button"} onClick={() => this.clickHandler(room)}>
+          <Text style={{ backgroundColor: 'green' }}>{room}</Text>
+        </VrButton>
+      )
+    ))
+    return buttons;
+  }
+
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text>
+            Room Selection
+          </Text>
+          <Text>
+            {this.state.room}
+          </Text>
+          {this.createRoomButtons(this.state.adjacentRooms)}
+        </View>
+
+
+        <View style={styles.greetingBox}>
+          <Text>
+            Room Info
+          </Text>
+          <Text>
+            {this.state.info}
+          </Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+export class InfoPanel extends React.Component {
   state = {
     room: house.House.roomName,
     info: house.House.info,
@@ -84,4 +141,5 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('HelloWorldVR', () => HelloWorldVR);
+AppRegistry.registerComponent('Buttons', () => Buttons);
+AppRegistry.registerComponent('InfoPanel', () => InfoPanel);
