3d0bdba03d75d6bf52251f0998a2ecdfb2b9623a
Sahaj Amatya <sahajamatya1@gmail.com>
2021-03-21 19:32:51 -0500

    HouseTourVR Complete with surge

--- client.js
+++ client.js
@@ -1,9 +1,4 @@
-// This file contains the boilerplate to execute your React app.
-// If you want to modify your application's content, start in "index.js"
-
 import { ReactInstance, Surface } from 'react-360-web';
-import WebVRPolyfill from 'webvr-polyfill';
-const polyfill = new WebVRPolyfill();
 
 function init(bundle, parent, options = {}) {
   const r360 = new ReactInstance(bundle, parent, {
@@ -12,26 +7,36 @@ function init(bundle, parent, options = {}) {
     ...options,
   });
 
-  const buttonsPanel = new Surface(400, 550, Surface.SurfaceShape.Flat);
-  buttonsPanel.setAngle(-0.6, 0.1);
+  const buttonsPanel = new Surface(
+    400,
+    400,
+    Surface.SurfaceShape.Flat
+  );
 
-  const infoPanel = new Surface(400, 550, Surface.SurfaceShape.Flat);
-  infoPanel.setAngle(0.6, 0);
+  buttonsPanel.setAngle(
+    -0.6,
+    0.1
+  );
 
   r360.renderToSurface(
-    r360.createRoot('Buttons', {}),
+    r360.createRoot('ConnectedButtonInfoPanel'),
     buttonsPanel
   );
 
-  r360.renderToSurface(
-    r360.createRoot('InfoPanel', {}),
-    infoPanel
+  const infoPanel = new Surface(
+    400,
+    400,
+    Surface.SurfaceShape.Flat
   );
 
-  // Render your app content to the default cylinder surface
+  infoPanel.setAngle(
+    0.6,
+    0.1
+  );
+
   r360.renderToSurface(
-    r360.createRoot('HelloWorldVR', { /* initial props */ }),
-    r360.getDefaultSurface()
+    r360.createRoot('ConnectedHouseInfoPanel'),
+    infoPanel
   );
 
   // Load the initial environment
--- index.js
+++ index.js
@@ -2,144 +2,148 @@ import React from 'react';
 import {
   asset,
   AppRegistry,
-  Environment,
+  NativeModules,
+  Image,
   StyleSheet,
   Text,
   View,
   VrButton
 } from 'react-360';
-import house from './data/houseData';
+import { connect, changeRoom } from './store';
 
-export class Buttons extends React.Component {
+const { AudioModule } = NativeModules;
+
+class AudioPanel extends React.Component {
+  playAmbientMusic() {
+    AudioModule.playEnvironmental({
+      source: asset('ambient.wav'),
+      volume: 0.3
+    });
+  }
+
+  stopAmbientMusic() {
+    AudioModule.stopEnvironmental();
+  }
+
+  render() {
+    return (
+      <View style={styles.audioPanel}>
+        <VrButton onClick={() => this.playAmbientMusic()}>
+          <Image style={{ height: 50, width: 50 }} source={asset('audioOn.png')} />
+        </VrButton>
+        <VrButton onClick={() => this.stopAmbientMusic()}>
+          <Image style={{ height: 50, width: 50 }} source={asset('audioOff.png')} />
+        </VrButton>
+      </View>
+    )
+  }
+}
+class HouseInfoPanel extends React.Component {
+  render() {
+    return (
+      <View>
+        <View style={styles.infoPanel}>
+          <Text style={styles.header}>Room Info</Text>
+          <Text style={{ fontSize: 20, textAlign: 'center', fontWeight: 'bold' }}>{this.props.info}</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+class Button extends React.Component {
   state = {
-    room: house.House.roomName,
-    info: house.House.info,
-    adjacentRooms: house.House.adjacentRooms
+    hover: false
   }
 
   clickHandler(roomSelection) {
-    this.setState({
-      room: house[`${roomSelection}`].roomName,
-      info: house[`${roomSelection}`].info,
-      adjacentRooms: house[`${roomSelection}`].adjacentRooms
-    });
-    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`));
+    changeRoom(roomSelection);
   }
+
+  render() {
+    return (
+      <VrButton style={this.state.hover ? styles.hover : styles.button}
+        onEnter={() => this.setState({ hover: true })}
+        onExit={() => this.setState({ hover: false })}
+        onClick={() => this.clickHandler(this.props.room)}>
+        <Text style={{ textAlign: 'center' }}>{this.props.room.split('_').join(' ')}</Text>
+      </VrButton>
+    )
+  }
+}
+
+export default class ButtonInfoPanel extends React.Component {
 
   createRoomButtons(adjacentRooms) {
     let rooms = adjacentRooms;
     let buttons = [];
 
     rooms.map(room => (
-      buttons.push(
-        <VrButton key={`${room}` + "-button"} onClick={() => this.clickHandler(room)}>
-          <Text style={{ backgroundColor: 'green' }}>{room}</Text>
-        </VrButton>
-      )
+      buttons.push(<Button key={`${room}` + '-button'} room={room} />)
     ))
+
     return buttons;
   }
 
   render() {
     return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text>
-            Room Selection
-          </Text>
-          <Text>
-            {this.state.room}
-          </Text>
-          {this.createRoomButtons(this.state.adjacentRooms)}
-        </View>
-
-
-        <View style={styles.greetingBox}>
-          <Text>
-            Room Info
-          </Text>
-          <Text>
-            {this.state.info}
-          </Text>
+      <View>
+        <View style={styles.buttonPanel}>
+          <Text style={styles.header}>Room Selection</Text>
+          {this.createRoomButtons(this.props.adjacentRooms)}
+          <AudioPanel />
         </View>
       </View>
     );
   }
 };
 
-export class InfoPanel extends React.Component {
-  state = {
-    room: house.House.roomName,
-    info: house.House.info,
-    adjacentRooms: house.House.adjacentRooms
-  }
-
-  clickHandler(roomSelection) {
-    this.setState({
-      room: house[`${roomSelection}`].roomName,
-      info: house[`${roomSelection}`].info,
-      adjacentRooms: house[`${roomSelection}`].adjacentRooms
-    });
-    Environment.setBackgroundImage(asset(`./360_${house[`${roomSelection}`].img}`));
-  }
-
-  createRoomButtons(adjacentRooms) {
-    let rooms = adjacentRooms;
-    let buttons = [];
-
-    rooms.map(room => (
-      buttons.push(
-        <VrButton key={`${room}` + "-button"} onClick={() => this.clickHandler(room)}>
-          <Text style={{ backgroundColor: 'green' }}>{room}</Text>
-        </VrButton>
-      )
-    ))
-    return buttons;
-  }
-
-  render() {
-    return (
-      <View style={styles.panel}>
-        <View style={styles.greetingBox}>
-          <Text>
-            Room Selection
-          </Text>
-          <Text>
-            {this.state.room}
-          </Text>
-          {this.createRoomButtons(this.state.adjacentRooms)}
-        </View>
-
-
-        <View style={styles.greetingBox}>
-          <Text>
-            Room Info
-          </Text>
-          <Text>
-            {this.state.info}
-          </Text>
-        </View>
-      </View>
-    );
-  }
-};
+const ConnectedButtonInfoPanel = connect(ButtonInfoPanel);
+const ConnectedHouseInfoPanel = connect(HouseInfoPanel);
 
 const styles = StyleSheet.create({
-  panel: {
-    // Fill the entire surface
-    width: 1000,
-    height: 600,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    justifyContent: 'space-between',
+  audioPanel: {
     flexDirection: 'row'
   },
-  greetingBox: {
-    padding: 20,
-    backgroundColor: '#000000',
-    borderColor: '#639dda',
-    borderWidth: 2,
+  infoPanel: {
+    width: 400,
+    height: 400,
+    opacity: 0.8,
+    backgroundColor: 'rgb(255, 200, 50)',
+    borderColor: 'rgb(255, 255, 255)',
+    borderWidth: 5,
+    borderRadius: 20
   },
+  buttonPanel: {
+    width: 400,
+    height: 400,
+    opacity: 0.8,
+    backgroundColor: 'rgb(255, 200, 50)',
+    borderColor: 'rgb(255, 255, 255)',
+    borderWidth: 5,
+    borderRadius: 20,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  button: {
+    width: 200,
+    backgroundColor: 'rgb(0, 0, 0)',
+    borderColor: 'rgb(255, 255, 255)',
+    borderWidth: 5,
+  },
+  hover: {
+    width: 200,
+    backgroundColor: 'rgb(0, 45, 72)',
+    borderColor: 'rgb(255, 255, 255)',
+    borderWidth: 5,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
 });
 
-AppRegistry.registerComponent('Buttons', () => Buttons);
-AppRegistry.registerComponent('InfoPanel', () => InfoPanel);
+AppRegistry.registerComponent('ConnectedButtonInfoPanel', () => ConnectedButtonInfoPanel);
+AppRegistry.registerComponent('ConnectedHouseInfoPanel', () => ConnectedHouseInfoPanel);
